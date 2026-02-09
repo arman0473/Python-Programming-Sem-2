425bd1d60822a7ce57bba9323296c5bd320b139d
Araman Mujavar <aramanmujawar3@gmail.com>
2026-02-09 15:35:49 +0530

    Create Activity1.py

---
+++ Experiment1/Activity1.py
@@ -0,0 +1,17 @@
+num1=int(input("Enter Number 1:"))
+num2=int(input("Enter Number 2:"))
+
+sum=num1+num2
+print("Addition=",sum)
+
+sub=num1-num2
+print("Substraction",sub)
+
+mul=num1*num2
+print("multiplication",mul)
+
+div=num1/num2
+print("Devision",div)
+
+mod=num1%num2
+print("Modulus",mod)
